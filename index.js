2b44bfdef599419909161aaa52ddf812eaf76e79
Ethan Liang <ehliang@uwaterloo.ca>
2016-05-06 21:11:26 -0400

    Server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use('/', express.static(path.join(__dirname, './')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+request.post({
+        url: 'https://www.google.ca',
+         body: "mes=heydude"
+         }, function(error, response, body){
+            console.log(body);
+    });
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
