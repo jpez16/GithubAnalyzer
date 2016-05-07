800d97121d021a8e7355bcdb07d0696a048ddebf
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-06 23:18:23 -0400

    comment cleanup

--- routes/code.js
+++ routes/code.js
@@ -1,21 +1,16 @@
 module.exports = function(router, request, config) {
 	router.get('/code', function(req, res){
 	var profileInfo = [];
-	//res.send('https://api.github.com/' + req.query.owner + '/' + 
-	//	'&client_id=' + config.CLIENT_ID+ '&client_secret=' + config.CLIENT_SECRET);
 	var url = 'https://api.github.com/users/' + req.query.owner + '?' + 
 			'client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET;
-	//res.send(url);
 	var getData = function(){	
 		request.get({
 			url: url,
 			headers: { 'user-agent': 'git-technetium' },
 			json: true
 		}, function(error, response, body) {
-			//if(!error && response.statusCode === 200) {
 			console.log(error);
 			res.send(body);
-			//}
 		});
 	};
 	getData();
