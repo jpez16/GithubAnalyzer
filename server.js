631483403e89fa101668f93a26505fe49d582d08
Ethan Liang <ehliang@uwaterloo.ca>
2016-05-06 23:27:49 -0400

    moved to server.js

--- server.js
+++ server.js
@@ -1,16 +1,56 @@
 var express = require('express'),
     request = require('request'),
-    config = require('./config');
+    config = require('./config'),
+    fs = require('fs'),
+	path = require('path'),
+	bodyParser = require('body-parser'), 
+	crypto = require('crypto');
 
 var app = express();
+
+
+app.use('/', express.static(path.join(__dirname, '')));
+app.use(bodyParser.json());
 
 var router = express.Router();
 
 require('./routes/code')(router, request, config);
 app.use('/api', router);
 
-app.use(express.static('./public'));
 
 app.listen(config.PORT, 'localhost', function() {
 	    console.log('Express server started on localhost: ' + config.PORT);
 });
+
+var initialSort = function(input){
+	if (newFile)
+	{
+
+	}
+
+	else(change)
+	{
+
+	}
+
+}
+
+var hashCheck = function(input){
+
+	var fd = fs.createReadStream('./testFileCompare/bootstrap.min.css');
+
+	var hash = crypto.createHash('sha1');
+	hash.setEncoding('hex');
+
+	fd.on('end', function() {
+	    hash.end();
+	    console.log(hash.read());
+	});
+
+	fd.pipe(hash);
+
+}; 
+
+var codeCheck = function(input){
+
+};
