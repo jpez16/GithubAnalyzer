e8c0725646310cc679b402c845eb80f48c7966dc
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-06 21:24:02 -0400

    html template

---
+++ js/main.js
@@ -0,0 +1,14 @@
+    var AppView = Backbone.View.extend({
+      // el - stands for element. Every view has a element associate in with HTML
+      //      content will be rendered.
+      el: '#container',
+      // It's the first function called when this view it's instantiated.
+      initialize: function(){
+        this.render();
+      },
+      // $el - it's a cached jQuery object (el), in which you can use jQuery functions
+      //       to push content. Like the Hello World in this case.
+      render: function(){
+        this.$el.html("Hello World");
+      }
+    });
